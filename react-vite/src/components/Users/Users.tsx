a41c8d04c5821ebd9aa9349cd9d818b5aeada880
Maximilian <retdetwet@gmail.com>
2024-10-10 20:47:33 +0300

    added filter/search users

--- react-vite/src/components/Users/Users.tsx
+++ react-vite/src/components/Users/Users.tsx
@@ -1,20 +1,27 @@
-import {memo, useEffect} from 'react'
-import {$users, fetchUsersFx} from './model.ts'
+import {ChangeEvent, memo, useEffect} from 'react'
+import {$filterText, $users, fetchUsersFx, filterTextChanged} from './model.ts'
 import {useUnit} from 'effector-react'
 import {Preloader} from '../common/Preloader'
 import s from './Users.module.scss'
 
 export const Users = memo(() => {
 
-    const [users, isLoading] = useUnit([$users, fetchUsersFx.pending])
+    const [users, isLoading, filterText, filterEvent] = useUnit([$users, fetchUsersFx.pending, $filterText, filterTextChanged])
 
     useEffect(() => {
         fetchUsersFx().finally(() => console.log('fetch finally!'))
     }, [])
 
+    const handleFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        filterEvent(e.target.value)
+    }
+
     if (isLoading) return <Preloader/>
 
-    return <div className={s.list}>
+    return <div>
+        <input value={filterText} onChange={handleFilter}/>
+        <div className={s.list}>
         {users.map((user) => <div key={user.name}>{user.name}</div>)}
+    </div>
     </div>
 })
--- react-vite/src/components/Users/model.ts
+++ react-vite/src/components/Users/model.ts
@@ -1,9 +1,10 @@
-import {createEffect, createEvent, createStore} from 'effector'
+import {createEffect, createEvent, createStore, sample} from 'effector'
 import {IUser} from './types.ts'
+import {debounce, not} from 'patronum'
 
 const BASE_URL = 'http://localhost:3000'
 
-export const fetchUsersFx = createEffect(async () => {
+export const fetchUsersFx = createEffect<void, IUser[], Error>(async () => {
     return await fetch(BASE_URL + '/users')
         .then((res) => res.json())
         .catch((error) => error)
@@ -15,7 +16,7 @@ export const usersChanged = createEvent<IUser[]>()
 export const $countUsers = createStore<number>(0)
 export const countUsersChanged = createEvent<number>()
 
-$users.on(usersChanged, (state, payload) => [...state, ...payload])
+$users.on(usersChanged, (_, payload) => payload)
 $countUsers.on(countUsersChanged, (_, payload) => payload)
 
 fetchUsersFx.done.watch(({result}) => {
@@ -23,4 +24,30 @@ fetchUsersFx.done.watch(({result}) => {
     usersChanged(result)
     countUsersChanged(result.length)
 })
+
+export const fetchFilteredUsersFx = createEffect<string, IUser[], Error>(async (name: string) => {
+    return await fetch(BASE_URL + '/users' + `?name=${name}`)
+        .then((res) => res.json())
+        .catch((error) => error)
+})
+
+export const $filterText = createStore<string>('')
+export const filterTextChanged = createEvent<string>()
+
+fetchFilteredUsersFx.done.watch(({result}) => {
+    usersChanged(result)
+})
+
+$filterText.on(filterTextChanged, (_, payload) => payload)
+
+const performFilter = debounce({source: filterTextChanged, timeout: 500})
+
+sample({
+    clock: performFilter,
+    source: $filterText,
+    filter: not(fetchFilteredUsersFx.pending),
+    target: fetchFilteredUsersFx
+})
+
+
 
