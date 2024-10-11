63a7e7d36ea2cd730a138f8b72db02ba9eb5fb7e
Maximilian <retdetwet@gmail.com>
2024-10-11 17:59:04 +0300

    added README.md file.

--- react-vite/vite.config.ts
+++ react-vite/vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
     strictPort: true,
     port: 5173,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  }
 })
